c33fed931071ed39afdfd837f8648826f935205c
RyanJHolt <44439777+RyanJHolt@users.noreply.github.com>
2020-02-01 17:02:11 +0000

    Revert "Revert "revert to be reverted""
    This reverts commit 523c7b6ddb1f158495dc5d30803dc7ef43e36d72.

---
+++ Assets/Script/Fire.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject bubbleBullet;
+    public int reload = 1;
+    public bool reloaded;
+    public bool reloading;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reloaded = true;
+    }
+
+    private void fire()
+    {
+        if (true){
+        Instantiate (bubbleBullet, gameObject.transform.position, gameObject.transform.rotation);
+        reloaded = false; 
+        reloading = false;
+        } 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reloaded == false & reloading == false)
+        {
+            Reload();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            fire();
+        }
+    }
+
+    IEnumerator Reload(){
+        yield return new WaitForSeconds(reload);
+        reloaded = true;
+    }
+}
