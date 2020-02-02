9ed1f5cafb0a52a257992a617abea6acfb3374cb
Cathal <cathalmgahan@gmail.com>
2020-02-02 01:09:12 +0000

    Find total number of plastics

--- Assets/Script/plastic.cs
+++ Assets/Script/plastic.cs
@@ -2,27 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class plastic : MonoBehaviour
-{
+public class plastic : MonoBehaviour {
     GameSession session;
+    [SerializeField] public int score = 1;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         session = FindObjectOfType<GameSession>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
             GameObject.Destroy(gameObject);
-            session.addPlastic(3);
+            session.addPlastic(score);
         }
     }
 }
